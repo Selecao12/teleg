48f4d2a7ef86e6e8e17783b24c547d79bc1836d7
ftp <nikiti1m_teleg@flareon.beget.ru>
2019-03-11 18:48:58 +0300

    Hook, db created

---
+++ .bash_history
@@ -0,0 +1,28 @@
+ll
+cd ..
+ll
+cd public_html
+ll
+composer
+git -version
+git -v
+git --v
+git --version 
+git init
+git branch 
+git add .
+git commit -m "Init"
+git remote add origin https://github.com/Selecao12/teleg.git
+git push -u origin master
+git branch 
+ll
+php composer.phar require longman/telegram-bot
+composer
+composer -update
+composer u
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+composer -v
+php composer.phar require longman/telegram-bot
---
+++ hook.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitalik
+ * Date: 11.03.2019
+ * Time: 20:35
+ */
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+// Получите токен у бота @BotFather
+$API_KEY = '775369493:AAG5XX1HTdFXCcA9p0HwAHGdcWAiqiuHOgg';
+
+// Получите свой User ID у бота @MyTelegramID_bot
+$USER_ID = '476237163';
+
+// Придумайте своему боту имя
+$BOT_NAME = "AuthBot";
+
+// Данные базы данных
+$mysql_credentials = [
+    'host'     => 'localhost',
+    'user'     => 'root',
+    'password' => '',
+    'database' => 'nikiti1m_teleg',
+];
+
+use Longman\TelegramBot\Telegram;
+use Longman\TelegramBot\TelegramLog;
+
+try {
+    // Инициализация бота
+    $telegram = new Telegram($API_KEY, $BOT_NAME);
+
+    // Подключение базы данных
+    $telegram->enableMySQL($mysql_credentials);
+
+    // Добавление папки commands,
+    // в которой будут лежать ваши личные комманды
+    $telegram->addCommandsPath(__DIR__ . "/commands");
+
+    // Добавление администратора бота
+    $telegram->enableAdmin((int)$USER_ID);
+
+    // Включение логов
+    TelegramLog::initUpdateLog($BOT_NAME . '_update.log');
+
+    // Опционально. Здесь вы можете указать кастомный объект update,
+    // чтобы поймать ошибки через var_dump.
+    //$telegram->setCustomInput("");
+
+    // Основной обработчик событий
+    $telegram->handle();
+
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // В случае неудачи будет выведена ошибка
+    var_dump($e);
+}
