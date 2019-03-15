69ddef3c4ecaee50dc6993d0d34f0d03fbb89c3f
Witalii Nikitin <witalikalive@gmail.com>
2019-03-15 21:31:49 +0500

    Add keyboard

---
+++ commands/CancelCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vitalik
+ * Date: 15.03.2019
+ * Time: 21:30
+ */
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+
+class CancelCommand
+{
+
+}
