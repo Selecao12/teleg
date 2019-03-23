fd67b76bf69ec9652678c9d55b4238868b97e88a
Witalii Nikitin <witalikalive@gmail.com>
2019-03-24 00:34:01 +0500

    Добавлена команда смены пароля. fix

--- commands/ChangepasswordCommand.php
+++ commands/ChangepasswordCommand.php
@@ -31,7 +31,7 @@ class ChangepasswordCommand extends UserCommand
 
         if (($inputData = $this->getInputData($text)) !== false &&
             ($userRow = $this->getUserRow($userId, $inputData['login'])) !== false &&
-            $this->checkOldPassword($inputData['old_password'], $userRow['password_hash']) &&
+            $this->checkOldPassword($inputData['old_password'], $userRow['password_hash']) &
             $this->checkNewPassword($inputData['new_password']) &&
             $this->changePassword($userId, $inputData['login'], $inputData['new_password'])
         ) {
